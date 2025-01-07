ae44d4fc5c4aff5180dc10ead1bbf6aea7e4ceaa
ariancruz <thor.sac.arian@gmail.com>
2025-01-06 23:56:30 -0300

    updatev18

--- projects/data-table-prime/src/lib/services/abstract.http.service.ts
+++ projects/data-table-prime/src/lib/services/abstract.http.service.ts
@@ -1,4 +1,4 @@
-import {HttpClient, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {LazyLoadData, LazyResultData} from '../models';
